1e2e134535548bb5dfb29b85a9f66fff72a20652
Danny Davidson <chief@onceknown.com>
2025-06-27 20:28:55 -0500

    the chaining, there's the chaining, not sure if this will work

--- build/lib/index.d.ts
+++ build/lib/index.d.ts
@@ -14,54 +14,53 @@ export interface Color {
     disable(): void;
     setTheme(theme: any): void;
 
-    strip(str: string): Color;
-    stripColors(str: string): Color;
+    strip: (str: string) => Color | Color;
+    stripColors: (str: string) => Color | Color;
 
-    black(str: string): Color;
-    red(str: string): Color;
-    green(str: string): Color;
-    yellow(str: string): Color;
-    blue(str: string): Color;
-    magenta(str: string): Color;
-    cyan(str: string): Color;
-    white(str: string): Color;
-    gray(str: string): Color;
+    black: (str: string) => Color | Color;
+    red: (str: string) => Color | Color;
+    green: (str: string) => Color | Color;
+    yellow: (str: string) => Color | Color;
+    blue: (str: string) => Color | Color;
+    magenta: (str: string) => Color | Color;
+    cyan: (str: string) => Color | Color;
+    white: (str: string) => Color | Color;
+    gray: (str: string) => Color | Color;
 
-    brightRed(str: string): Color;
-    brightGreen(str: string): Color;
-    brightYellow(str: string): Color;
-    brightBlue(str: string): Color;
-    brightMagenta(str: string): Color;
-    brightCyan(str: string): Color;
-    brightWhite(str: string): Color;
+    brightRed: (str: string) => Color | Color;
+    brightGreen: (str: string) => Color | Color;
+    brightYellow: (str: string) => Color | Color;
+    brightBlue: (str: string) => Color | Color;
+    brightMagenta: (str: string) => Color | Color;
+    brightCyan: (str: string) => Color | Color;
+    brightWhite: (str: string) => Color | Color;
 
-    bgBlack(str: string): Color;
-    bgRed(str: string): Color;
-    bgGreen(str: string): Color;
-    bgYellow(str: string): Color;
-    bgBlue(str: string): Color;
-    bgMagenta(str: string): Color;
-    bgCyan(str: string): Color;
-    bgWhite(str: string): Color;
-    bgGray(str: string): Color;
+    bgBlack: (str: string) => Color | Color;
+    bgRed: (str: string) => Color | Color;
+    bgGreen: (str: string) => Color | Color;
+    bgYellow: (str: string) => Color | Color;
+    bgBlue: (str: string) => Color | Color;
+    bgMagenta: (str: string) => Color | Color;
+    bgCyan: (str: string) => Color | Color;
+    bgWhite: (str: string) => Color | Color;
+    bgGray: (str: string) => Color | Color;
 
-    bgBrightRed(str: string): Color;
-    bgBrightGreen(str: string): Color;
-    bgBrightYellow(str: string): Color;
-    bgBrightBlue(str: string): Color;
-    bgBrightMagenta(str: string): Color;
-    bgBrightCyan(str: string): Color;
-    bgBrightWhite(str: string): Color;
+    bgBrightRed: (str: string) => Color | Color;
+    bgBrightGreen: (str: string) => Color | Color;
+    bgBrightYellow: (str: string) => Color | Color;
+    bgBrightBlue: (str: string) => Color | Color;
+    bgBrightMagenta: (str: string) => Color | Color;
+    bgBrightCyan: (str: string) => Color | Color;
+    bgBrightWhite: (str: string) => Color | Color;
 
-    reset(str: string): Color;
-    bold(str: string): Color;
-    dim(str: string): Color;
-    italic(str: string): Color;
-    underline(str: string): Color;
-    inverse(str: string): Color;
-    hidden(str: string): Color;
-    strikethrough(str: string): Color;
-
+    reset: (str: string) => Color | Color;
+    bold: (str: string) => Color | Color;
+    dim: (str: string) => Color | Color;
+    italic: (str: string) => Color | Color;
+    underline: (str: string) => Color | Color;
+    inverse: (str: string) => Color | Color;
+    hidden: (str: string) => Color | Color;
+    strikethrough: (str: string) => Color | Color;
 }
 
 export type ColorSupportLevel = {
