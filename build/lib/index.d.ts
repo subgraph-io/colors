5d5cefd2a82ab0d3d885b76ce9aa36f926ca240a
Danny Davidson <chief@onceknown.com>
2025-06-27 20:46:54 -0500

    add gemini and use for generating type from JS, with a fix I think it might be correct

--- build/lib/index.d.ts
+++ build/lib/index.d.ts
@@ -1,73 +1,82 @@
-// Type definitions for Colors.js 1.2
+
+// Type definitions for Colors.js 1.4
 // Project: https://github.com/Marak/colors.js
 // Definitions by: Bart van der Schoor <https://github.com/Bartvds>, Staffan Eketorp <https://github.com/staeke>
-// Definitions: https://github.com/Marak/colors.js
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
-declare const value: Color;
-export default value;
+export as namespace colors;
+export = colors;
 
-export interface Color {
+declare const colors: Color;
+
+interface Color {
     (text: string): string;
 
-    supportsColor(): ColorSupportLevel;
+    strip: Color;
+    stripColors: Color;
+
     enable(): void;
     disable(): void;
+
     setTheme(theme: any): void;
 
-    strip: (str: string) => Color | Color;
-    stripColors: (str: string) => Color | Color;
+    // Main methods
+    bold: Color;
+    dim: Color;
+    italic: Color;
+    underline: Color;
+    inverse: Color;
+    hidden: Color;
+    strikethrough: Color;
 
-    black: (str: string) => Color | Color;
-    red: (str: string) => Color | Color;
-    green: (str: string) => Color | Color;
-    yellow: (str: string) => Color | Color;
-    blue: (str: string) => Color | Color;
-    magenta: (str: string) => Color | Color;
-    cyan: (str: string) => Color | Color;
-    white: (str: string) => Color | Color;
-    gray: (str: string) => Color | Color;
+    // Colors
+    black: Color;
+    red: Color;
+    green: Color;
+    yellow: Color;
+    blue: Color;
+    magenta: Color;
+    cyan: Color;
+    white: Color;
+    gray: Color;
+    grey: Color;
 
-    brightRed: (str: string) => Color | Color;
-    brightGreen: (str: string) => Color | Color;
-    brightYellow: (str: string) => Color | Color;
-    brightBlue: (str: string) => Color | Color;
-    brightMagenta: (str: string) => Color | Color;
-    brightCyan: (str: string) => Color | Color;
-    brightWhite: (str: string) => Color | Color;
+    // Bright colors
+    brightRed: Color;
+    brightGreen: Color;
+    brightYellow: Color;
+    brightBlue: Color;
+    brightMagenta: Color;
+    brightCyan: Color;
+    brightWhite: Color;
 
-    bgBlack: (str: string) => Color | Color;
-    bgRed: (str: string) => Color | Color;
-    bgGreen: (str: string) => Color | Color;
-    bgYellow: (str: string) => Color | Color;
-    bgBlue: (str: string) => Color | Color;
-    bgMagenta: (str: string) => Color | Color;
-    bgCyan: (str: string) => Color | Color;
-    bgWhite: (str: string) => Color | Color;
-    bgGray: (str: string) => Color | Color;
+    // Background colors
+    bgBlack: Color;
+    bgRed: Color;
+    bgGreen: Color;
+    bgYellow: Color;
+    bgBlue: Color;
+    bgMagenta: Color;
+    bgCyan: Color;
+    bgWhite: Color;
+    bgGray: Color;
+    bgGrey: Color;
 
-    bgBrightRed: (str: string) => Color | Color;
-    bgBrightGreen: (str: string) => Color | Color;
-    bgBrightYellow: (str: string) => Color | Color;
-    bgBrightBlue: (str: string) => Color | Color;
-    bgBrightMagenta: (str: string) => Color | Color;
-    bgBrightCyan: (str: string) => Color | Color;
-    bgBrightWhite: (str: string) => Color | Color;
+    // Bright background colors
+    bgBrightRed: Color;
+    bgBrightGreen: Color;
+    bgBrightYellow: Color;
+    bgBrightBlue: Color;
+    bgBrightMagenta: Color;
+    bgBrightCyan: Color;
+    bgBrightWhite: Color;
 
-    reset: (str: string) => Color | Color;
-    bold: (str: string) => Color | Color;
-    dim: (str: string) => Color | Color;
-    italic: (str: string) => Color | Color;
-    underline: (str: string) => Color | Color;
-    inverse: (str: string) => Color | Color;
-    hidden: (str: string) => Color | Color;
-    strikethrough: (str: string) => Color | Color;
+    // Other methods
+    reset: Color;
+    trap: Color;
+    zalgo: Color;
+    america: Color;
+    rainbow: Color;
+    random: Color;
+    zebra: Color;
 }
-
-export type ColorSupportLevel = {
-    level: number;
-    hasBasic: boolean;
-    has256: boolean;
-    has16m: boolean;
-};
-
-
